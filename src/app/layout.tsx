58c2452cd3127987738d1e21465840b402a3cd51
astrxnomo <lgiraldoo@unal.edu.co>
2024-12-09 18:30:48 -0500

    Update layout component to apply background styles to body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
       lang="es"
       className={`min-h-full ${inter.className} ${poppins.variable}`}
     >
-      <body>
+      <body className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
         <Header />
         <main>{children}</main>
         <Footer />
