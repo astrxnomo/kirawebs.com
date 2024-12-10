491d74c864959b55962cd8fbc2c773946b5ea1f7
astrxnomo <dev@felipego.com>
2024-12-09 22:45:56 -0500

    Update image domains in next.config and clean up header animation styles in globals.css

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'file.notion.so'],
+    domains: ['img.notionusercontent.com'],
   },
 };
 
