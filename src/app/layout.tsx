3362c6723f981c6763ecf08b4e4886df306db106
astrxnomo <lgiraldoo@unal.edu.co>
2025-01-01 23:05:37 -0500

    feat: update analytics script key for improved tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
       <head>
         <script
           src="https://analytics.ahrefs.com/analytics.js"
-          data-key="p69hfR6vzM0HlUiDJW6wEA"
+          data-key="xD+P5nM2R6bSyazQv6TWtQ"
           async
         ></script>
       </head>
