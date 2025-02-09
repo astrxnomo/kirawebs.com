3a7cd6acd685d115ff5d0c0ddc3247d1a684b3d1
astrxnomo <dev@felipego.com>
2025-02-09 13:31:44 -0500

    refactor: update contact section with more descriptive service highlights

--- src/components/home/contact-section.tsx
+++ src/components/home/contact-section.tsx
@@ -10,7 +10,7 @@ export function ContactSection() {
         <div className="flex flex-col justify-center gap-5">
           <div className="flex flex-col items-center text-center lg:items-start lg:text-left">
             <h2 className="text-4xl font-bold tracking-tighter md:text-5xl">
-              Contáctanos
+              ¡Contáctanos!
             </h2>
             <p className="mt-4 text-muted-foreground">
               Estamos aquí para ayudarte. Completa el formulario para ponerte en
@@ -21,27 +21,30 @@ export function ContactSection() {
             <div className="flex items-center gap-6 lg:mx-0">
               <Check className="size-4 text-primary" />
               <div className="flex flex-col gap-1">
-                <p>Fácil de usar</p>
+                <p>Diseño Personalizado</p>
                 <p className="text-sm text-muted-foreground">
-                  Hemos hecho que sea fácil de usar y entender.
+                  Creamos páginas web únicas que reflejan la identidad de tu
+                  marca.
                 </p>
               </div>
             </div>
             <div className="flex items-center gap-6 lg:mx-0">
               <Check className="size-4 text-primary" />
               <div className="flex flex-col gap-1">
-                <p>Rápido y confiable</p>
+                <p>Optimización SEO</p>
                 <p className="text-sm text-muted-foreground">
-                  Hemos hecho que sea rápido y confiable.
+                  Nuestras páginas están optimizadas para mejorar tu visibilidad
+                  en motores de búsqueda.
                 </p>
               </div>
             </div>
             <div className="flex items-center gap-6 lg:mx-0">
               <Check className="size-4 text-primary" />
               <div className="flex flex-col gap-1">
-                <p>Moderno y atractivo</p>
+                <p>Soporte Continuo</p>
                 <p className="text-sm text-muted-foreground">
-                  Hemos hecho que sea moderno y atractivo.
+                  Ofrecemos soporte constante para asegurar que tu sitio web
+                  funcione perfectamente.
                 </p>
               </div>
             </div>
--- src/components/home/contact/contact-form.tsx
+++ src/components/home/contact/contact-form.tsx
@@ -27,7 +27,7 @@ export function ContactForm() {
 
   return (
     <Card className="mx-auto w-full max-w-lg">
-      <CardContent className="pt-6">
+      <CardContent>
         <form className="w-full space-y-6" action={formAction}>
           <div className="space-y-2">
             <Label htmlFor="input-10">
